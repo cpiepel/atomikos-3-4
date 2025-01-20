a5ad47cd80e56de56cdd9b0bcd17bff8d4c06536
Carsten Piepel <c.piepel@lvm.de>
2025-01-20 10:45:30 +0100

    Initial commit

---
+++ src/main/java/com/example/atomikos/atomikos_3_4/Atomikos34Application.java
@@ -0,0 +1,13 @@
+package com.example.atomikos.atomikos_3_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atomikos34Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atomikos34Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/atomikos/atomikos_3_4/Atomikos34ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.atomikos.atomikos_3_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atomikos34ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
